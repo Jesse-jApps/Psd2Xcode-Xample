2a250306459a99abde85741110e986ba3fdfe60c
Jesse Hinrichsen <jesse@j-apps.com>
2014-10-12 21:13:55 +0200

    added all constraints except scrollview

--- SimpleXample-iPhone/SimpleXample-iPhone/ViewController.m
+++ SimpleXample-iPhone/SimpleXample-iPhone/ViewController.m
@@ -10,18 +10,55 @@
 
 @interface ViewController ()
 
+@property (nonatomic, weak) IBOutlet UIImageView *logo;
+
+@property (nonatomic, weak) IBOutlet UITextField *inputTextField;
+@property (nonatomic, weak) IBOutlet UITextField *outputTextField;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
+- (void)viewDidAppear:(BOOL)animated
+{
+	[self animateLogo:[NSNumber numberWithFloat:1.5]];
+	[self performSelector:@selector(animateLogo:) withObject:[NSNumber numberWithFloat:1] afterDelay:0.3];
+	
+}
+
+- (void)animateLogo:(NSNumber *)scale
+{
+	if (_logo) {
+		
+		[UIView beginAnimations:nil context:nil];
+		[UIView setAnimationDuration:0.3];
+		[UIView setAnimationCurve:UIViewAnimationCurveEaseOut];
+		
+		
+		_logo.transform = CGAffineTransformScale(CGAffineTransformIdentity, [scale floatValue], [scale floatValue]);
+
+		
+		[UIView commitAnimations];
+		
+	}
+}
+
+- (IBAction)slectableButton:(id)sender
+{
+	UIButton *button = (UIButton *)sender;
+	
+	[button setSelected:!button.selected];
+}
+
+- (IBAction)sendTextButton:(id)sender
+{
+	if (_inputTextField && _outputTextField) {
+		_outputTextField.text = _inputTextField.text;
+	}
 }
 
 @end
