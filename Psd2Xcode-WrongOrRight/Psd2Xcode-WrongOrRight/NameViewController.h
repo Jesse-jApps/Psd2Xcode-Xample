83f716e636e88bfc0d23bf372322222a4e2e49c2
Jesse Hinrichsen <jesse@j-apps.com>
2014-11-10 00:13:41 +0100

    Added new xample Project

---
+++ Psd2Xcode-WrongOrRight/Psd2Xcode-WrongOrRight/NameViewController.h
@@ -0,0 +1,13 @@
+//
+//  NameViewController.h
+//  Psd2Xcode-WrongOrRight
+//
+//  Created by jesse on 09.11.14.
+//  Copyright (c) 2014 j-apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NameViewController : UIViewController
+
+@end
---
+++ Psd2Xcode-WrongOrRight/Psd2Xcode-WrongOrRightTests/Psd2Xcode_WrongOrRightTests.m
@@ -0,0 +1,40 @@
+//
+//  Psd2Xcode_WrongOrRightTests.m
+//  Psd2Xcode-WrongOrRightTests
+//
+//  Created by jesse on 09.11.14.
+//  Copyright (c) 2014 j-apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Psd2Xcode_WrongOrRightTests : XCTestCase
+
+@end
+
+@implementation Psd2Xcode_WrongOrRightTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
