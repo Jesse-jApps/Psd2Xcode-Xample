7e8e35cd420f0474a9c21919f4362ddeb847cb1f
Jesse Hinrichsen <Jesse Hinrichsen>
2015-06-27 17:20:21 +0200

    new landscape example

---
+++ Landscape-iPhone/Landscape-iPhone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Landscape-iPhone
+//
+//  Created by Jesse Hinrichsen on 27.06.15.
+//  Copyright (c) 2015 Jesse Hinrichsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
